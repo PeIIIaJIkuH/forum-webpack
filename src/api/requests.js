0662d6d87d1e0382fa1107a2bf0c32c1df4c9594
Yerkhan <baigal.erhan@gmail.com>
2021-02-10 18:23:56 +0600

    fixed requests.js, app works

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,7 +1,7 @@
-import * as axios from 'axios'
+import axios from 'axios'
 
 const defaultAxios = axios.create({
-	baseURL: 'http://localhost:8081',
+	baseURL: 'http://localhost:8081/api/',
 	withCredentials: true
 })
 
