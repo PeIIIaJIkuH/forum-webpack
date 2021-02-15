1808722384f383de047a5f372696845698701f99
Yerkhan <baigal.erhan@gmail.com>
2021-02-15 08:45:24 +0600

    updated rate buttons, blur after click

--- src/components/Posts/Post/Rate.jsx
+++ src/components/Posts/Post/Rate.jsx
@@ -5,23 +5,27 @@ import {DownOutlined, UpOutlined} from '@ant-design/icons'
 
 const Rate = ({isAuth, setRating, post}) => {
 	const isRatedUp = post.userRating === 1,
-		isRatedDown = post.userRating === -1
+		isRatedDown = post.userRating === -1,
+		upRef = React.createRef(),
+		downRef = React.createRef()
 
 	const onUpClick = () => {
 		setRating(post.id, 1)
+		upRef.current.blur()
 	}
 
 	const onDownClick = () => {
 		setRating(post.id, -1)
+		downRef.current.blur()
 	}
 
 	return (
 		<div>
-			<Button className={`${s.up} ${isRatedUp && s.ratedUp}`}
-					icon={<UpOutlined/>} disabled={!isAuth} onClick={onUpClick}/>
+			<Button className={`${s.up} ${isRatedUp && s.ratedUp}`} icon={<UpOutlined/>} disabled={!isAuth}
+					onClick={onUpClick} ref={upRef}/>
 			<div className={s.ratingNumber}>{post.postRating}</div>
-			<Button className={`${s.down} ${isRatedDown && s.ratedDown}`}
-					icon={<DownOutlined/>} disabled={!isAuth} onClick={onDownClick}/>
+			<Button className={`${s.down} ${isRatedDown && s.ratedDown}`} icon={<DownOutlined/>} disabled={!isAuth}
+					onClick={onDownClick} ref={downRef}/>
 		</div>
 	)
 }
