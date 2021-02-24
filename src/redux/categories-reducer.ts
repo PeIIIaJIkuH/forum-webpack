999dd38c40a1af9e2ee2d30547d2f7dc689aa270
Yerkhan <baigal.erhan@gmail.com>
2021-02-24 19:58:02 +0600

    updated categories-reducer.ts

--- src/redux/categories-reducer.ts
+++ src/redux/categories-reducer.ts
@@ -1,10 +1,7 @@
 import {categoriesAPI} from '../api/requests'
 import {ThunkDispatch} from 'redux-thunk'
-import {State} from './store'
+import {ActionTypes, State} from './store'
 import {Category} from '../types/types'
-
-const SET_CATEGORIES = 'categories/SET_CATEGORIES',
-	SET_SELECTED_CATEGORIES = 'categories/SET_SELECTED_CATEGORIES'
 
 type InitialState = {
 	all: Category[] | null
@@ -15,36 +12,29 @@ const initialState: InitialState = {
 	selected: null
 }
 
-type Action = SetCategoriesAC | SetSelectedCategoriesAC
+type Action = ActionTypes<typeof categoriesActions>
 
 const categoriesReducer = (state = initialState, action: Action) => {
 	switch (action.type) {
-		case SET_CATEGORIES:
+		case 'categories/SET_CATEGORIES':
 			return {...state, all: action.categories}
-		case SET_SELECTED_CATEGORIES:
+		case 'categories/SET_SELECTED_CATEGORIES':
 			return {...state, selected: action.categories}
 		default:
 			return state
 	}
 }
 
-type SetCategoriesAC = {
-	type: typeof SET_CATEGORIES
-	categories: Category[] | null
+const categoriesActions = {
+	setCategoriesAC: (categories: Category[] | null) => ({
+		type: 'categories/SET_CATEGORIES',
+		categories
+	} as const),
+	setSelectedCategoriesAC: (categories: string[] | null) => ({
+		type: 'categories/SET_SELECTED_CATEGORIES',
+		categories
+	} as const)
 }
-const setCategoriesAC = (categories: Category[] | null): SetCategoriesAC => ({
-	type: SET_CATEGORIES,
-	categories
-})
-
-type SetSelectedCategoriesAC = {
-	type: typeof SET_SELECTED_CATEGORIES
-	categories: string[] | null
-}
-const setSelectedCategoriesAC = (categories: string[] | null): SetSelectedCategoriesAC => ({
-	type: SET_SELECTED_CATEGORIES,
-	categories
-})
 
 type Dispatch = ThunkDispatch<State, unknown, Action>
 
@@ -52,12 +42,12 @@ export type RequestCategories = () => void
 export const requestCategories = () => async (dispatch: Dispatch) => {
 	const data = await categoriesAPI.all()
 	const arr = data && data.data ? data.data : null
-	await dispatch(setCategoriesAC(arr))
+	await dispatch(categoriesActions.setCategoriesAC(arr))
 }
 
 export type SetSelectedCategories = (categories: string[] | null) => void
 export const setSelectedCategories = (categories: string[] | null) => async (dispatch: Dispatch) => {
-	dispatch(setSelectedCategoriesAC(categories))
+	dispatch(categoriesActions.setSelectedCategoriesAC(categories))
 }
 
 export default categoriesReducer
