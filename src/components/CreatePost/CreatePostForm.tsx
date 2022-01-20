1fd135fe4773fb39bf34da5dc29f08702f776646
PeIIIaJIkuH <baigal.erhan@gmail.com>
2022-01-21 00:12:28 +0600

    handled image update and removal

--- src/components/CreatePost/CreatePostForm.tsx
+++ src/components/CreatePost/CreatePostForm.tsx
@@ -31,8 +31,8 @@ type Props = {
 export const CreatePostForm: FC<Props> = observer(({isFetching, setIsFetching}) => {
 	const location = useLocation(),
 		history = useHistory(),
-		[isImage, setIsImage] = useState(postsState.editing?.isImage || false),
-		[imagePath, setImagePath] = useState(postsState.editing?.imagePath || '')
+		[formData, setFormData] = useState<FormData | null>(null),
+		[imageState, setImageState] = useState<-1 | 0 | 1>(0)
 
 	useEffect(() => {
 		postsState.fetchAllCategories().then()
@@ -43,12 +43,27 @@ export const CreatePostForm: FC<Props> = observer(({isFetching, setIsFetching}) 
 
 	type obj = { title: string, content: string, categories: string[] }
 	const onSubmit = async ({title, content, categories}: obj) => {
+		let imagePath = postsState.editing?.imagePath, isImage = postsState.editing?.isImage
+		if (postsState.editing && imageState && isImage) {
+			await postsAPI.deleteImage(postsState.editing.id)
+			imagePath = ''
+			isImage = false
+		}
+		if (formData) {
+			const {data, status} = await postsAPI.uploadImage(formData, {
+				headers: {'content-type': 'multipart/form-data'},
+			})
+			if (status) {
+				imagePath = data
+				isImage = true
+			}
+		}
 		setIsFetching(true)
 		title = title.trim()
 		content = content?.replace(/\n+/, '\n').split('\n').map(line => line.trim()).join('\n')
 		categories = categories.map(tag => tag.trim())
 		if (!postsState.editing) {
-			const {status} = await postsAPI.createPost(title, content, categories, isImage, imagePath)
+			const {status} = await postsAPI.createPost(title, content, categories, isImage || false, imagePath || '')
 			if (status) {
 				setIsFetching(false)
 				history.push('/')
@@ -79,13 +94,13 @@ export const CreatePostForm: FC<Props> = observer(({isFetching, setIsFetching}) 
 				<Input autoFocus/>
 			</Form.Item>
 			<Form.Item label='Content' name='content' initialValue={postsState.editing?.content}
-			           rules={!isImage ? [defaultValidator('Content')] : undefined}
+			           rules={!formData ? [defaultValidator('Content')] : undefined}
 			>
 				<TextArea allowClear autoSize={{minRows: 3, maxRows: 10}} showCount/>
 			</Form.Item>
 			<Form.Item label='Image' name='image'>
-				<ImageUpload setImagePath={setImagePath} setIsImage={setIsImage}
-				             defaultFileList={postsState.editing?.isImage && defaultFileList}
+				<ImageUpload defaultFileList={postsState.editing?.isImage && defaultFileList} setFormData={setFormData}
+				             setImageState={setImageState}
 				/>
 			</Form.Item>
 			<Form.Item label='Categories' name='categories' rules={[defaultValidator('Categories')]}
--- src/components/CreatePost/ImageUpload.tsx
+++ src/components/CreatePost/ImageUpload.tsx
@@ -3,78 +3,41 @@ import {InboxOutlined} from '@ant-design/icons'
 import Upload, {RcFile} from 'antd/lib/upload'
 import message from 'antd/lib/message'
 import {observer} from 'mobx-react-lite'
-import postsState from '../../store/postsState'
-import {postsAPI} from '../../api/posts'
 
 type Props = {
-	setIsImage: (isImage: boolean) => void
-	setImagePath: (path: string) => void
 	defaultFileList: any
+	setFormData: (formData: FormData | null) => void
+	setImageState: (isImage: -1 | 0 | 1) => void
 }
 
-export const ImageUpload: FC<Props> = observer(({setIsImage, setImagePath, defaultFileList}) => {
-	const [fileList] = useState([])[0],
-		setProgress = useState(0)[1]
+export const ImageUpload: FC<Props> = observer(({defaultFileList, setFormData, setImageState}) => {
+	const [fileList] = useState([])[0]
 
 	const beforeUpload = (file: RcFile) => {
-		const isCorrectType = file.type === 'image/png' || file.type === 'image/jpg' || file.type === 'image/jpeg' || file.type === 'image/gif'
-		if (!isCorrectType) {
-			message.error(`${file.name} is not an image of required type!`).then()
+		if (file.size / 1024 / 1024 >= 20) {
+			message.error('Image must be less than 20 megabytes.').then()
+			return false
 		}
-		const isLt20M = file.size / 1024 / 1024 < 20
-		if (!isLt20M) {
-			message.error('Image must smaller than 20MB!').then()
-		}
-		return isCorrectType && isLt20M
+		return true
 	}
 
-	const progressData = {
-		strokeColor: {
-			'0%': '#108ee9',
-			'100%': '#87d068',
-		},
-		strokeWidth: 3,
-		format: (percent: any) => `${parseFloat(percent.toFixed(2))}%`,
-	}
-
-	const customRequest = async ({file, onProgress, onSuccess, onError}: any) => {
+	const customRequest = async ({file, onSuccess}: any) => {
 		const formData = new FormData()
-		const config = {
-			headers: {'content-type': 'multipart/form-data'},
-			onUploadProgress: (event: any) => {
-				const percent = parseFloat(Math.floor((event.loaded / event.total) * 100).toFixed(2))
-				setProgress(percent)
-				if (percent === 100) {
-					setTimeout(() => setProgress(0), 1000)
-				}
-				onProgress({percent: (event.loaded / event.total) * 100})
-			},
-		}
 		formData.append('image', file)
-		try {
-			const {data, status} = await postsAPI.uploadImage(formData, config)
-			if (status) {
-				setIsImage(true)
-				setImagePath(data)
-				onSuccess()
-			}
-		} catch (err) {
-			onError(err)
-		}
+		setFormData(formData)
+		setImageState(1)
+		onSuccess()
 	}
 
-	const onRemove = async () => {
-		const {status} = await postsAPI.deleteImage(postsState.editing?.id!)
-		if (status) {
-			setIsImage(false)
-			setImagePath('')
-		}
+	const onRemove = () => {
+		setFormData(null)
+		setImageState(-1)
 	}
 
 	return (
-		<Upload.Dragger name='image' fileList={fileList} beforeUpload={beforeUpload} progress={progressData}
-		                customRequest={customRequest} listType='picture' maxCount={1} onRemove={onRemove}
-		                defaultFileList={defaultFileList}
+		<Upload.Dragger name='image' fileList={fileList} beforeUpload={beforeUpload} maxCount={1} listType='picture'
+		                customRequest={customRequest} defaultFileList={defaultFileList} accept='.png,.jpg,.jpeg,.gif'
+		                onRemove={onRemove}
 		>
 			<p className='ant-upload-drag-icon'>
 				<InboxOutlined/>
