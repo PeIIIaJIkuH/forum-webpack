db1df9c7a014ffbf432baa78610c9d7a2619cb94
PeIIIaJIkuH <baigal.erhan@gmail.com>
2022-02-02 19:01:45 +0600

    updated user api

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,14 +1,41 @@
 import {ApiPromise} from '../types'
-import {defaultAxios} from './index'
+import {updatingAxios} from './index'
 
-const fetchUser = (userId: number): ApiPromise => defaultAxios.get(`user/${userId}`)
+const fetchUser = (userId: number): ApiPromise => updatingAxios.get(`user/${userId}`)
 
-const fetchNotifications = (): ApiPromise => defaultAxios.get('notifications')
+const fetchDefaultNotifications = (): ApiPromise => updatingAxios.get('notifications')
 
-const clearNotifications = (): ApiPromise => defaultAxios.delete('notifications/delete/')
+const deleteDefaultNotifications = (): ApiPromise => updatingAxios.delete('notifications/delete/')
+
+const requestPromotionToModerator = (): ApiPromise => updatingAxios.post('request/add')
+
+const deleteRequestPromotionToModerator = (): ApiPromise => updatingAxios.delete('request/delete')
+
+const getRequestPromotionToModerator = (): ApiPromise => updatingAxios.get('request')
+
+const fetchRoleNotifications = (): ApiPromise => updatingAxios.get('user/notifications/role')
+
+const deleteRoleNotifications = (): ApiPromise => updatingAxios.delete('user/notifications/role/delete')
+
+const fetchReportNotifications = (): ApiPromise => updatingAxios.get('user/notifications/report')
+
+const deleteReportNotifications = (): ApiPromise => updatingAxios.delete('user/notifications/report/delete')
+
+const fetchPostNotifications = (): ApiPromise => updatingAxios.get('user/notifications/post')
+
+const deletePostNotifications = (): ApiPromise => updatingAxios.delete('user/notifications/post/delete')
 
 export const userAPI = {
 	fetchUser,
-	fetchNotifications,
-	clearNotifications,
+	fetchDefaultNotifications,
+	deleteDefaultNotifications,
+	requestPromotionToModerator,
+	deleteRequestPromotionToModerator,
+	getRequestPromotionToModerator,
+	fetchRoleNotifications,
+	deleteRoleNotifications,
+	fetchReportNotifications,
+	deleteReportNotifications,
+	fetchPostNotifications,
+	deletePostNotifications,
 }
