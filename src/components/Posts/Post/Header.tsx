9216af919c91899da68b4adc29317a6e98839c52
PeIIIaJIkuH <baigal.erhan@gmail.com>
2022-02-02 18:58:58 +0600

    added delete/report/edit options

--- src/components/Posts/Post/Header.tsx
+++ src/components/Posts/Post/Header.tsx
@@ -1,42 +1,65 @@
 import React, {FC, useState} from 'react'
 import s from '../Posts.module.css'
 import Button from 'antd/lib/button'
-import {DeleteOutlined, EditOutlined, MoreOutlined} from '@ant-design/icons'
+import {DeleteOutlined, EditOutlined, MoreOutlined, WarningOutlined} from '@ant-design/icons'
 import {Link, useHistory} from 'react-router-dom'
 import Popover from 'antd/lib/popover'
-import {IPost} from '../../../types'
+import {EUserRole, IPost} from '../../../types'
 import message from 'antd/lib/message'
 import {observer} from 'mobx-react-lite'
 import postsState from '../../../store/postsState'
 import authState from '../../../store/authState'
+import {adminAPI} from '../../../api/admin'
+import {moderatorAPI} from '../../../api/moderator'
 
 type Props = {
 	post: IPost
-	postPage?: boolean
 }
 
-export const Header: FC<Props> = observer(({post, postPage}) => {
-	const history = useHistory(),
-		[loading, setLoading] = useState(false),
-		[visible, setVisible] = useState(false)
-
-	const onDelete = async () => {
-		setVisible(false)
-		setLoading(true)
-		const status = await postsState.deletePost(post.id)
-		if (status) {
-			if (postPage) {
-				history.push('/')
-			}
-		} else {
-			setLoading(false)
-			message.error('Can not delete post!')
-		}
-	}
+export const Header: FC<Props> = observer(({post}) => {
+	const history = useHistory()
+	const [visible, setVisible] = useState(false)
+	const isAuthor = authState.user?.id === post.author.id
+	const isModerator = authState.role === EUserRole.moderator
+	const isAdmin = authState.role === EUserRole.admin
+	const [deleteLoading, setDeleteLoading] = useState(false)
+	const [reportLoading, setReportLoading] = useState(false)
 
 	const onEdit = async () => {
 		setVisible(false)
 		history.push(`/edit?id=${post.id}`)
+	}
+
+	const onDelete = async (type: 'author' | 'moderator' | 'admin') => {
+		setVisible(false)
+		let status: boolean
+		setDeleteLoading(true)
+		if (type === 'author') {
+			status = await postsState.deletePost(post.id)
+		} else if (type === 'moderator') {
+			status = (await moderatorAPI.deletePost(post.id)).status
+		} else {
+			status = (await adminAPI.deletePost(post.id)).status
+		}
+		setDeleteLoading(false)
+		if (status) {
+			message.success('post was deleted successfully')
+			await postsState.fetchAllPosts()
+		} else {
+			message.error('can not delete post')
+		}
+	}
+
+	const onReport = async () => {
+		setVisible(false)
+		setReportLoading(true)
+		const {status} = await moderatorAPI.createReport(authState.user?.id!, post.id)
+		setReportLoading(false)
+		if (status) {
+			message.success('post was reported successfully')
+		} else {
+			message.error('can not report post')
+		}
 	}
 
 	const handleVisibleChange = (visible: boolean) => {
@@ -45,8 +68,27 @@ export const Header: FC<Props> = observer(({post, postPage}) => {
 
 	const content = (
 		<div className={s.actions}>
-			<Button type='text' icon={<EditOutlined/>} onClick={onEdit}/>
-			<Button danger type='link' icon={<DeleteOutlined/>} onClick={onDelete} loading={loading}/>
+			{isAuthor ? (
+				<>
+					<Button type='text' icon={<EditOutlined/>} onClick={onEdit}/>
+					<Button danger type='link' icon={<DeleteOutlined/>} loading={deleteLoading}
+					        onClick={onDelete.bind(null, 'author')}
+					/>
+				</>
+			) : isModerator ? (
+				<>
+					<Button danger type='link' icon={<DeleteOutlined/>} loading={deleteLoading}
+					        onClick={onDelete.bind(null, 'moderator')}
+					/>
+					<Button danger type='link' icon={<WarningOutlined/>} loading={reportLoading}
+					        onClick={onReport}
+					/>
+				</>
+			) : isAdmin ? (
+				<Button danger type='link' icon={<DeleteOutlined/>} loading={deleteLoading}
+				        onClick={onDelete.bind(null, 'admin')}
+				/>
+			) : null}
 		</div>
 	)
 
@@ -55,7 +97,7 @@ export const Header: FC<Props> = observer(({post, postPage}) => {
 			<Link className={s.title} to={`/post/${post.id}`}>
 				{post.title}
 			</Link>
-			{authState.user?.id === post.author.id && (
+			{(isAuthor || isAdmin || isModerator) && (
 				<div className={s.more}>
 					<Popover trigger='click' placement='left' visible={visible} onVisibleChange={handleVisibleChange}
 					         content={content}
