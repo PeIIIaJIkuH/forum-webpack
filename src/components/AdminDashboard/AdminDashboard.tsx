62f3fd0e67fa1aa689ec0d2171bd2bcfb9e0b6b0
PeIIIaJIkuH <baigal.erhan@gmail.com>
2022-02-02 18:52:26 +0600

    updated admin dashboard

--- src/components/AdminDashboard/AdminDashboard.tsx
+++ src/components/AdminDashboard/AdminDashboard.tsx
@@ -1,7 +1,51 @@
-import React, {FC} from 'react'
+import React, {FC, useEffect} from 'react'
+import Tabs from 'antd/lib/tabs'
+import {Helmet} from 'react-helmet'
+import s from './AdminDashboard.module.css'
+import adminState from '../../store/adminState'
+import authState from '../../store/authState'
+import {Error403} from '../common/errors/Error403'
+import {observer} from 'mobx-react-lite'
+import {Requests} from './Requests'
+import {EUserRole} from '../../types'
+import {Reports} from './Reports'
+import {Categories} from './Categories'
+import {Moderators} from './Moderators'
 
-export const AdminDashboard: FC = () => {
-	return (
-		<div>Admin Dashboard</div>
+const {TabPane} = Tabs
+
+export const AdminDashboard: FC = observer(() => {
+	const getTab = (name: string) => (
+		<div className={s.tab}>
+			{name}
+		</div>
 	)
-}
+
+	useEffect(() => {
+		adminState.fetchAll().then()
+	}, [])
+
+	if (!authState.user || authState.role !== EUserRole.admin) {
+		return <Error403/>
+	}
+
+	return (
+		<div>
+			<Helmet><title>Admin Dashboard | forume</title></Helmet>
+			<Tabs centered>
+				<TabPane tab={getTab('Requests')} key='requests'>
+					<Requests/>
+				</TabPane>
+				<TabPane tab={getTab('Reports')} key='reports'>
+					<Reports/>
+				</TabPane>
+				<TabPane tab={getTab('Moderators')} key='moderators'>
+					<Moderators/>
+				</TabPane>
+				<TabPane tab={getTab('Categories')} key='categories'>
+					<Categories/>
+				</TabPane>
+			</Tabs>
+		</div>
+	)
+})
