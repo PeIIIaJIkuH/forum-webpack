dd913b92d6bb9be1f39f09123f8079cf43bb1e30
PeIIIaJIkuH <baigal.erhan@gmail.com>
2022-02-02 18:59:14 +0600

    added another axios instance

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,27 @@
 import axios from 'axios'
+import authState from '../store/authState'
 
 export const defaultAxios = axios.create({
 	baseURL: 'https://localhost:8081/api/',
-	// baseURL: 'http://139.59.211.109:8081/api/',
 	withCredentials: true,
+	timeout: 10000,
 })
 
-defaultAxios.interceptors.response.use(r => r.data, e => e.response?.data || {state: false})
+defaultAxios.interceptors.response.use(
+	(r) => r.data,
+	(e) => e.response?.data || {state: false, data: null},
+)
+
+export const updatingAxios = axios.create({
+	baseURL: 'https://localhost:8081/api/',
+	withCredentials: true,
+	timeout: 10000,
+})
+
+updatingAxios.interceptors.response.use(
+	async (r) => {
+		await authState.fetchUserData()
+		return r.data
+	},
+	(e) => e.response?.data || {state: false, data: null},
+)
